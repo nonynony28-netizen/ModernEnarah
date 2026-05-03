894f7b2e2215b8967715661b1aed1901b6c4f8ea
nonynony28-netizen <nonynony28@gmail.com>
2026-05-04 01:58:26 +0200

    Refactor routing to use BrowserRouter

--- App.tsx
+++ App.tsx
@@ -1,27 +1,30 @@
-import { Routes, Route } from 'react-router-dom'
-import Layout from './Layout'
-import Home from './pages/Home'
-import Products from './pages/Products'
-import Brands from './pages/Brands'
-import Projects from './pages/Projects'
-import About from './pages/About'
-import Branches from './pages/Branches'
-import Contact from './pages/Contact'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./Layout";
+
+import Home from "./Home";
+import Products from "./Products";
+import Brands from "./Brands";
+import Projects from "./Projects";
+import About from "./About";
+import Branches from "./Branches";
+import Contact from "./Contact";
 
 function App() {
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/brands" element={<Brands />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/branches" element={<Branches />} />
-        <Route path="/contact" element={<Contact />} />
-      </Route>
-    </Routes>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/brands" element={<Brands />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/branches" element={<Branches />} />
+          <Route path="/contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
