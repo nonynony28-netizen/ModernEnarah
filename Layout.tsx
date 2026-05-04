e120dc382d42bbca2e65000b2dedd36fc1092f21
nonynony28-netizen <nonynony28@gmail.com>
2026-05-04 02:06:41 +0200

    Update Layout.tsx

--- Layout.tsx
+++ Layout.tsx
@@ -1,15 +1,9 @@
-import { Outlet } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import { Outlet } from "react-router-dom";
 
 export default function Layout() {
   return (
-    <div className="min-h-screen bg-brand-darker">
-      <Navbar />
-      <main>
-        <Outlet />
-      </main>
-      <Footer />
-    </div>
-  )
+    <>
+      <Outlet />
+    </>
+  );
 }
