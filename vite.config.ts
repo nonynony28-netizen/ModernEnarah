a95268742950a359cebe2dd7c432704118c9bbb6
nonynony28-netizen <nonynony28@gmail.com>
2026-05-04 02:19:14 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   base: "./",
 
@@ -12,12 +11,6 @@ export default defineConfig({
 
   server: {
     port: 3000,
-  },
-
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
   },
 
   build: {
