f7cc0c1052741101f18c91baf401269c8ad26746
nonynony28-netizen <nonynony28@gmail.com>
2026-05-04 11:32:13 +0200

    Update Home component layout and content

--- Home.tsx
+++ Home.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <h1 style={{ color: "white", textAlign: "center" }}>
-      الموقع شغال ✅
-    </h1>
+    <div style={{ padding: "40px", color: "white" }}>
+      <h1 style={{ fontSize: "28px" }}>الإنارة الحديثة</h1>
+      <p>مرحبا بك في الموقع</p>
+    </div>
   );
 }
