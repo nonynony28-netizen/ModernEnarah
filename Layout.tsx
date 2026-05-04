950d722e94652d8144d96bcd374f25b1ec600754
nonynony28-netizen <nonynony28@gmail.com>
2026-05-04 12:45:26 +0200

    Wrap Outlet in main for layout adjustment
    Added main wrapper around Outlet for spacing under Navbar.

--- Layout.tsx
+++ Layout.tsx
@@ -1,11 +1,14 @@
-import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
+import { Outlet } from "react-router-dom";
 
 export default function Layout() {
   return (
     <>
       <Navbar />
-      <Outlet />
+      {/* مهم: يعطي مساحة تحت النافبار */}
+      <main className="pt-24">
+        <Outlet />
+      </main>
     </>
   );
 }
